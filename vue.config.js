03433fcad1648de8ffd33cd280fc376b98eecca0
Mjiarong <mjiarong@163.com>
2022-07-07 21:27:35 +0800

    创建项目框架，完成与前端的通信

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,
+  devServer: {
+      proxy: {
+        '^/api': {
+          target: 'http://localhost:3000',
+          ws: true,
+          changeOrigin: true,
+        },
+      },
+    },
 })
